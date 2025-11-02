6a4fb68d347e1bf9bf92bc5df1c2211723b939c8
Lily Ana Valley <mslilyvalley@icloud.com>
2025-11-02 14:41:54 -0500

    added /help options for /start /stop /forcestop

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -14,7 +14,7 @@ pub async fn help(ctx: Context<'_>) -> Result<(), Error> {
         .field("/serverinfo", "Display server information", false)
         .field("/start", "Start the PalWorld server", false)
         .field("/stop", "Stop the PalWorld server", false)
-        .field("/force_stop", "Force stop the PalWorld server", false)
+        .field("/forcestop", "Force stop the PalWorld server", false)
         .field("/help", "Show this help message", false)
         .color(0x7289da)
         .footer(serenity::CreateEmbedFooter::new(concat!(
